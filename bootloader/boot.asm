d1777d24d01acd88940db08886540745b7c3a93f
epicalex4444 <alexroe@email.com>
2021-06-18 21:38:04 +1000

    added things to get to to protected mode

--- bootloader/boot.asm
+++ bootloader/boot.asm
@@ -13,17 +13,58 @@
 [org 0x7C00]
 [bits 16]
 
+;move stack out of the way
 mov bp, 0x7C00
-mov sp, bp ;move stak out of the way
+mov sp, bp
 
 ;bios automatically loads boot drive into dl
 mov al, 0x04
 call read_disk
 
+;need add memory map here since it uses bios interupts
+
+;bios interupts shouldn't be used form here onwards
+cli
+
+;enable the A20 line
+in al, 0x92
+or al, 0x02
+out 0x92, al
+
+;load the gdt
+lgdt [gdt_descriptor]
+
+;enable protected mode bit in the cr0 register
+mov eax, cr0
+or eax, 0x1
+mov cr0, eax
+
+;far jump avoid pipelining issues
+jmp CODE_SEG:pm
+
+%include "print_16.asm" ;relies on vga, which is not always supported
+%include "read_disk.asm"
+%include "gdt.asm"
+
+[bits 32]
+
+pm:
+
+;disable bios interupts because they can't be used in protected mode
+cli
+
+;update stack registers
+mov ax, DATA_SEG
+mov ds, ax
+mov ss, ax
+mov es, ax
+mov fs, ax
+mov gs, ax
+
+;hang
 jmp $
 
-%include "print.asm" ;relies on vga, which is not always supported
-%include "read_disk.asm"
+%include "print_32.asm" ;relies on vga, which is not always supported
 
 ;make file 512 bytes and add boot signature(both needed to run)
 times 510 - ($ - $$) db 0x00
