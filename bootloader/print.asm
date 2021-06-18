7aaa1213b4f410389e40cd249426a8d8fc13592b
epicalex4444 <alexroe@email.com>
2021-06-18 13:06:18 +1000

    style tidy up and some small optimisations

--- bootloader/print.asm
+++ bootloader/print.asm
@@ -1,9 +1,11 @@
 ;mode: any
-;use type: call
 ;params: bx = address of null terminated string
 ;returns: none
 print:
-    pusha
+    push ax
+    push bx
+    push cx
+    push dx
     push es
 
     ;to display char write at 0xB8000 + 0x02 * (row * 0x50 + col)
@@ -12,89 +14,75 @@ print:
     ;because the address is to large for 16 but use (es)segment register
     ;address es:bx = es << 4 + bx
 
-    mov dx, 0xB800
-    mov es, dx ;load 0xB800 into the segment register
-    mov dh, 0x0F ;load white on black style
-    push bx
-    call get_cursor
-    shl bx, 0x0001 ;multiply on 2 because vga uses 2 bytes per char
-    mov cx, bx ;save vga memory address
-    pop bx
-    .loop:
-        mov dl, [bx] ;move the charecter into dl
-        test dl, dl ;check if char is 0 which means end of string
-        jz .exit
-        cmp dl, 0x0A ;check for newline char
-        jz .newline
-        push bx
-        mov bx, cx ;mov vga memory to bx, only regesiter bx works for this
+    mov dx, 0xB800      ;
+    mov es, dx          ;load 0xB800 into the segment register
+    mov dh, 0x0F        ;load white on black style
+    call get_cursor     ;
+    shl ax, 0x0001      ;multiply on 2 because vga uses 2 bytes per char
+    .loop:              ;
+        mov dl, [bx]    ;move the charecter into dl
+        test dl, dl     ;check if char is 0 which means end of string
+        jz .exit        ;
+        cmp dl, 0x0A    ;check for newline char
+        jz .newline     ;
+        push bx         ;
+        mov bx, ax      ;mov vga memory to bx, only regesiter bx works for this
         mov [es:bx], dx ;write char into video memory
-        pop bx
-        inc bx ;next char
-        add cx, 0x0002 ;next memory location write
-        jmp .loop
-    .newline:
-        push ax
-        push dx
-        mov ax, cx ;load divdedend
-        mov dl, 0xA0 ;load divisor
-        div dl ;divide, ah=remainder, al=quotient
-        shr ax, 0x08 ;make ax = only the remainder
-        sub cx, ax ;minus remainder from address(makes x = 0)
-        add cx, 0xA0 ;move down 1 line
-        inc bx ;next char
-        pop dx
-        pop ax
-        jmp .loop
-    .exit:
-        shr cx, 0x0001
-        mov bx, cx
-        call set_cursor
-        pop es
-        popa
-        ret
+        pop bx          ;
+        inc bx          ;next char
+        add ax, 0x0002  ;next memory location write
+        jmp .loop       ;
+    .newline:           ;
+        push dx         ;
+        mov cx, ax      ;copy memory address for later
+        mov dl, 0xA0    ;load divisor
+        div dl          ;divide, ah=remainder, al=quotient
+        shr ax, 0x08    ;make ax = only the remainder
+        sub cx, ax      ;minus remainder from address(makes x = 0)
+        add cx, 0xA0    ;move down 1 line
+        mov ax, cx      ;move memory adress back to ax
+        inc bx          ;next char
+        pop dx          ;
+        jmp .loop       ;
+    .exit:              ;
+        shr ax, 0x0001  ;divide vga memory address by 2
+        mov bx, ax      ;mov it into bx
+        call set_cursor ;set the cursor to where it should be
+            push es     ;
+            push dx     ;
+            push cx     ;
+            push bx     ;
+            push ax     ;
+        ret             ;
 
 ;mode: any
-;use type: call
 ;params: none
-;returns:
-;   bx = cursor pos
+;returns: ax = cursor pos
 get_cursor:
     push dx
-    push ax
 
     ;cursor low is port 0x03D4, index 0x0F
     ;cursor high is port 0x03D4, index 0x0E
     ;to use the port output the index then
     ;use port 0x03D5 to set/read the value
 
-    mov dx, 0x03D4 ;misc vga I/O port
+    mov dx, 0x03D4 ;load port
+    mov al, 0x0E   ;load index
+    out dx, al     ;output index for cursor high
+    mov dx, 0x03D5 ;load port
+    in al, dx      ;input cursor high
+    shl ax, 0x0008 ;move to the high register
+    mov dx, 0x03D4 ;load port
+    mov al, 0x0F   ;load index
+    out dx, al     ;output index for cursor low
+    mov dx, 0x03D5 ;load port
+    in al, dx      ;input cursor low
 
-    mov al, 0x0E
-    out dx, al ;output index for cursor high
-    
-    inc dl
-    in al, dx ;input cursor high
-    
-    shl ax, 8 ;move to the high register
-
-    dec dl
-    mov al, 0x0F
-    out dx, al ;output index for cursor low
-
-    inc dl
-    in al, dx ;input cursor low
-    
-    mov bx, ax
-
-    pop ax
     pop dx
     ret
 
 ;mode: any
-;use type: call
-;params:
-;   bx = cursor pos
+;params: bx = cursor pos
 ;returns: none
 set_cursor:
     push ax
@@ -106,22 +94,18 @@ set_cursor:
     ;to use the port output the index then
     ;inc the port to set/read the value
 
-    mov dx, 0x03D4 ;misc vga I/O port
-
-    mov al, 0x0F
-    out dx, al ;output index for cursor low
-
-    inc dl
-    mov al, bl
-    out dx, al ;output cursor low
-
-    dec dl
-    mov al, 0x0E
-    out dx, al ;output index for cursor high
-    
-    inc dl
-    mov al, bh
-    out dx, al ;output cursor high
+    mov dx, 0x03D4 ;load port
+    mov al, 0x0F   ;load index
+    out dx, al     ;output index for cursor low
+    mov dx, 0x03D5 ;load port
+    mov al, bl     ;move bl into al(only al works)
+    out dx, al     ;output cursor low
+    mov dx, 0x03D4 ;load port
+    mov al, 0x0E   ;load index
+    out dx, al     ;output index for cursor high
+    mov dx, 0x03D5 ;load port
+    mov al, bh     ;move bl into al(only al works)
+    out dx, al     ;output cursor high
     
     pop dx
     pop bx
