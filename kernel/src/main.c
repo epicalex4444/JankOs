52bcceae75ad27df80aaf883d07817391f478ed2
epicalex4444 <alexroe@email.com>
2021-07-08 15:12:22 +1000

    formatting

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -1,15 +1,16 @@
 #include "attributes.h"
 #include "vga.h"
 #include "print.h"
+#include "types.h"
 
 NORETURN void _start(void) {
     //setup vga
-    //TODO - make sure tons of other settings are correct such as text mode, and blinking disabled
     clear_screen();
     set_cursor_pos(0);
     show_cursor(14, 15);
-
+    
     i8 str[] = "Hello, World!";
     print_string(str);
-    while (1);
+
+    while (true);
 }
