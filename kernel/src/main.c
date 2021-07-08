01f1b0f1251deba8a16f9401cce7f4527633d1df
epicalex4444 <alexroe@email.com>
2021-07-08 14:43:51 +1000

    updated changd function name

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -1,11 +1,15 @@
 #include "attributes.h"
 #include "vga.h"
+#include "print.h"
 
 NORETURN void _start(void) {
     //setup vga
     //TODO - make sure tons of other settings are correct such as text mode, and blinking disabled
     clear_screen();
-    set_cursor(0);
+    set_cursor_pos(0);
     show_cursor(14, 15);
+
+    i8 str[] = "Hello, World!";
+    print_string(str);
     while (1);
 }
