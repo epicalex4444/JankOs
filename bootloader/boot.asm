204499f532f569a94393436c101a27463d97006f
epicalex4444 <alexroe@email.com>
2021-07-09 11:55:46 +1000

    sector comparison was broken due to saving al on the stack

--- bootloader/boot.asm
+++ bootloader/boot.asm
@@ -73,23 +73,23 @@ jz error.long_mode  ;
 ;loads rest of the bootloader and the kernel
 ;capped at 255 sectors
 mov dl, [BOOT_DISK]     ;disk
-mov ah, 0xFF            ;sector count
-xor dh, dh              ;head
+mov dh, 0xFF            ;sector count
 xor ch, ch              ;cylinder
 mov cl, 2               ;sector, starts at 1
 mov bx, 0x7E00          ;base address
 loadLoop:               ;
-    cmp ah, 1           ;error if sectors = 1
+    cmp dh, 1           ;error if sectors = 1
     je error.disk_read  ;
-    mov al, ah          ;move sectors into al
-    push ax             ;save sector count
+    mov al, dh          ;move sectors into al
+    push dx             ;save sector count
+    xor dh, dh          ;head
     mov ah, 2           ;interupt code
     int 0x13            ;call bios interupt
-    pop ax              ;return sector count
-    dec ah              ;decrement sector count
+    pop dx              ;return sector count
+    dec dh              ;decrement sector count
     jc loadLoop         ;check no error was returned
     dec al              ;decrement al so the comparison works
-    cmp al, ah          ;check sectors read = sectors requested
+    cmp al, dh          ;check sectors read = sectors requested
     jne loadLoop        ;
 
 ;setup identity paging for the first 2 megabytes
