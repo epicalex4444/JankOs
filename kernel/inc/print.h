ba98643ccf48f66a28901f39922b3cb0abf72b52
epicalex4444 <alexroe@email.com>
2021-07-13 01:31:10 +1000

    added binary printing

--- kernel/inc/print.h
+++ kernel/inc/print.h
@@ -7,12 +7,19 @@
 
 #include "types.h"
 
-#define POS_MAX 1999
-#define NEXT_POS(pos) ((pos) == POS_MAX ? 0 : (pos) + 1)
+#define POS_MAX 1999 ///< last posible position to write to
+#define NEXT_POS(pos) ((pos) == POS_MAX ? 0 : (pos) + 1) ///< gets the next position to write to
 
 void print_string(i8* str);
+
 void print_hex(void* num, u8 size);
 void print_hex_8(u8 num, u16 pos);
 void print_hex_16(u16 num, u16 pos);
 void print_hex_32(u32 num, u16 pos);
 void print_hex_64(u64 num, u16 pos);
+
+void print_binary(void* num, u8 size);
+void print_binary_8(u8 num, u16 pos);
+void print_binary_16(u16 num, u16 pos);
+void print_binary_32(u32 num, u16 pos);
+void print_binary_64(u64 num, u16 pos);
--- kernel/src/print.c
+++ kernel/src/print.c
@@ -132,3 +132,118 @@ void print_hex_64(u64 num, u16 pos) {
     }
     set_cursor_pos(pos);
 }
+
+/**
+ * @brief prints out hex of a data type
+ * @param num void* to print out as binary
+ * @param size how many bits the data type is
+ * @details Starts at cursor position.
+ *          Cursor is updated to be after the last char,
+ *          wraps around to start if it gets to the end.
+ */
+void print_binary(void* num, u8 size) {
+    u16 pos = get_cursor_pos();
+    write_char('0', WHITE, BLACK, pos);
+    pos = NEXT_POS(pos);
+    write_char('b', WHITE, BLACK, pos);
+    pos = NEXT_POS(pos);
+
+    switch (size) {
+        case 8:
+            print_binary_8(*((u8*)num), pos);
+            break;
+        case 16:
+            print_binary_16(*((u16*)num), pos);
+            break;
+        case 32:
+            print_binary_32(*((u32*)num), pos);
+            break;
+        case 64:
+            print_binary_64(*((u64*)num), pos);
+            break;
+    }
+}
+
+/**
+ * @brief prints out u8 as binary
+ * @param num value to print a binary
+ * @param pos position to start printing from
+ * @details Cursor is updated to be after the last char,
+ *          wraps around to start if it gets to the end.
+ */
+void print_binary_8(u8 num, u16 pos) {
+    u8 mask = 0x80;
+    while (mask != 0) {
+        if (mask & num) {
+            write_char('1', WHITE, BLACK, pos);
+        } else {
+            write_char('0', WHITE, BLACK, pos);
+        }
+        mask >>= 1;
+        pos = NEXT_POS(pos);
+    }
+    set_cursor_pos(pos);
+}
+
+/**
+ * @brief prints out u16 as binary
+ * @param num value to print a binary
+ * @param pos position to start printing from
+ * @details Cursor is updated to be after the last char,
+ *          wraps around to start if it gets to the end.
+ */
+void print_binary_16(u16 num, u16 pos) {
+    u16 mask = 0x8000;
+    while (mask != 0) {
+        if (mask & num) {
+            write_char('1', WHITE, BLACK, pos);
+        } else {
+            write_char('0', WHITE, BLACK, pos);
+        }
+        mask >>= 1;
+        pos = NEXT_POS(pos);
+    }
+    set_cursor_pos(pos);
+}
+
+/**
+ * @brief prints out u32 as binary
+ * @param num value to print a binary
+ * @param pos position to start printing from
+ * @details Cursor is updated to be after the last char,
+ *          wraps around to start if it gets to the end.
+ */
+void print_binary_32(u32 num, u16 pos) {
+    u32 mask = 0x80000000;
+    while (mask != 0) {
+        if (mask & num) {
+            write_char('1', WHITE, BLACK, pos);
+        } else {
+            write_char('0', WHITE, BLACK, pos);
+        }
+        mask >>= 1;
+        pos = NEXT_POS(pos);
+    }
+    set_cursor_pos(pos);
+}
+
+/**
+ * @brief prints out u64 as binary
+ * @param num value to print a binary
+ * @param pos position to start printing from
+ * @details Cursor is updated to be after the last char,
+ *          wraps around to start if it gets to the end.
+ */
+void print_binary_64(u64 num, u16 pos) {
+    u64 mask = 0x8000000000000000;
+    while (mask != 0) {
+        if (mask & num) {
+            write_char('1', WHITE, BLACK, pos);
+        } else {
+            write_char('0', WHITE, BLACK, pos);
+        }
+        mask >>= 1;
+        pos = NEXT_POS(pos);
+    }
+    set_cursor_pos(pos);
+}
