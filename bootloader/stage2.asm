85d9a453a5281ef5469e8f2e13129f2a9dec9675
epicalex4444 <alexroe@email.com>
2021-07-22 05:06:57 +1000

    moved stack setting to kernel

--- bootloader/stage2.asm
+++ bootloader/stage2.asm
@@ -179,12 +179,6 @@ mov ss, ax
 ;disable interupts
 cli
 
-;sets up stack, at the top of available space
-;above is bios a bios area, below is the kernel
-;stack grows downwards
-mov esp, 0x9FC00
-mov ebp, 0x9FC00
-
 ;jump to kernel (jumps to kernel_entry.asm)
 jmp 0x8000
 
