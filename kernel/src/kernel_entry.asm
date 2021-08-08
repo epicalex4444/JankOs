f66f17a8e93d562f0c75ca4a0595232bb9799b4f
epicalex4444 <alexroe@email.com>
2021-08-08 16:18:59 +1000

    changed to using 64 bit registers stack and base

--- kernel/src/kernel_entry.asm
+++ kernel/src/kernel_entry.asm
@@ -7,8 +7,8 @@ _start:
 ;sets up stack, at the top of available space
 ;above is bios a bios area, below is the kernel
 ;stack grows downwards
-mov esp, 0x9FC00
-mov ebp, 0x9FC00
+mov rsp, 0x9FC00
+mov rbp, 0x9FC00
 
 ;jumps to _start function in main.c
 [extern start_kernel]
