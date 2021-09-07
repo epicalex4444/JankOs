a79f49fb1ef34389f23a51d494ed05f12ee9afad
epicalex4444 <alexroe@email.com>
2021-09-07 20:22:43 +1000

    added always_inline attribute

--- kernel/inc/attributes.h
+++ kernel/inc/attributes.h
@@ -8,3 +8,4 @@
 #define NORETURN __attribute__((noreturn)) ///< tell gcc that a function won't ever return
 #define PACKED __attribute__((packed))  ///< forces structs to have no empty space
 #define NAKED __attribute__((naked)) ///< makes it so gcc doesn't setup stack frame
+#define ALWAYS_INLINE __attribute__((always_inline)) ///< forces function to be inlined
